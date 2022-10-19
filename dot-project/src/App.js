e76b1bed73ba428e1ba2ab6a447b016a47786f84
Imren More <98141213+imrenmore@users.noreply.github.com>
2022-10-19 16:54:12 -0700

    Update App.js
    got rid of image error

--- dot-project/src/App.js
+++ dot-project/src/App.js
@@ -7,27 +7,22 @@ import five9logo from "./images/Five9-black-logo.jpg";
 
 // File must be imported to
 import SampleText from "./components/SampleText";
+import SampleDots from "./components/SampleDots";
 
 function App() {
   return (
     <div className="homepage">
-
       <h1 className="home-header">
-
-        <img className="logo" src={five9logo} />
-       
-
-
- 
-  
+        <img className= "logo" src= {five9logo} alt = ""/>
+      
       </h1>
       <div className="homepage-body">
         <SampleText />
         <div className="dot-container">
+        <SampleDots></SampleDots>
           <div className="sample-dot" />
           <div className="sample-dot2" />
-          <div className="sample-dot" />
-          <div className="sample-dot2" />
+
           </div>
         <div className="legend">Legend</div>
         <div>Green: Available</div>
